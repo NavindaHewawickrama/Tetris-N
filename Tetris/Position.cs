071db76794eeb747a665e54d090addaf91021087
Navinda Hewawickrama <124261092+NavindaHewawickrama@users.noreply.github.com>
2024-05-17 06:33:33 +0530

    Add project files.

---
+++ Tetris/Position.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Tetris
+{
+    class Position
+    {
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+
+        public Position(int column, int row)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+
+
+}
